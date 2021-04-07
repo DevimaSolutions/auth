3600d28fdc563af475626a974b3fac98426ebc6f
Mykola Pavlenchyk <mornadowork@gmail.com>
2021-04-07 18:40:09 +0300

    feat: Implemented custom storage option

--- src/types/IAuthOptions.ts
+++ src/types/IAuthOptions.ts
@@ -1,7 +1,9 @@
 import type { AxiosInstance, AxiosResponse } from 'axios';
+import type { IStorage } from 'src/storage';
 import type { IAuthResult, IUser } from './IUser';
 export default interface IAuthOptions {
   axiosInstance?: AxiosInstance;
+  storage?: IStorage;
   signIn(email: string, password: string): Promise<AxiosResponse<IAuthResult>>;
   signOut(authToken: string): Promise<void>;
   refreshToken(refreshToken: string): Promise<AxiosResponse<IAuthResult>>;
